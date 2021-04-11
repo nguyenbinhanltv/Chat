bb3b5ed042ac9ee1d469050203757bf53387abe5
duc <hohuynhduca7bh123@gmail.com>
2021-04-11 17:10:27 +0700

    chat room and chat message

--- src/App.js
+++ src/App.js
@@ -121,18 +121,32 @@ function ChatRoom() {
   }
 
   return (
-    <div className="w-1/2 h-4/5 mt-3 border-2 m-auto">
-      <div>
-        {/* call ChatMessage Component for each new message */}
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
-        <span ref={dummy}></span>
+    <div className="w-1/2 h-4/5 mt-1 border  m-auto">
+      <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+        <div>
+          {/* call ChatMessage Component for each new message */}
+          {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+           <span ref={dummy}></span>
+        </div>
+      <div className="relative flex">
+         <span className="absolute inset-y-0 flex items-center top-4 left-3">
+            <button type="button" className="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none mt-2">
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6 text-gray-600">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"></path>
+               </svg>
+            </button>
+         </span>
+         <form onSubmit={sendMessage} className="w-11/12 mt-6 ml-4 flex ">
+            <input type="text" value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Write Something" className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 bg-gray-200 rounded-full py-3 pl-12" />
+            <button type="submit" disabled={!formValue} className="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none ml-4">
+               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-6 w-6 transform rotate-90">
+                  <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+               </svg>
+            </button>
+        </form>
       </div>
-
-      <form onSubmit={sendMessage}>
-        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Say something" />
-        <button type="submit" disabled={!formValue}>send</button>
-      </form>
     </div>
+  </div>
   )
 }
 
@@ -140,14 +154,14 @@ function ChatMessage(props) {
   const { user, body, uid, photoURL, createdAt } = props.message;
 
     return (
-      <div className="flex items-end">
+      <div className="flex items-end ml-4 ">
         <div >
           <img src={photoURL || 'https://i.imgur.com/rFbS5ms.png'} alt="{user}'s pfp" className="rounded-full w-8 h-8" />
         </div>
-        <div className="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-          <p>{user}</p>
+        <div className="flex flex-col mt-8 space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
+          {/* <p>{user}</p> */}
           <p className="px-4 py-2 rounded-lg inline-block bg-gray-300 text-gray-600">{body}</p>
-      </div>
+        </div>
       </div>
   )
 }
